6759324ef98a73dd346f5e982cc43c54c00f2f94
seid ahmetovic <107319842+seyyy0@users.noreply.github.com>
2026-05-05 11:36:37 +0200

    Import Wasi function 'fd_read', setup props

--- src/wasmWasiMain/kotlin/KotlinREPL.kt
+++ src/wasmWasiMain/kotlin/KotlinREPL.kt
@@ -1,3 +1,10 @@
+import kotlin.wasm.unsafe.Pointer
+import kotlin.wasm.unsafe.UnsafeWasmMemoryApi
+import kotlin.wasm.unsafe.withScopedMemoryAllocator
+
 fun main() {
-    println("Hello from Kotlin via WASI")
+    println("Hello from Kotlin via WASI. REPL program is being initialized.")
 }
+
+@WasmImport("wasi_snapshot_preview1", "fd_read")
+private external fun wasiRawFdRead(fd: Int, iovs_ptr: Int, iovs_len: Int, nread_ptr: Int): Int
