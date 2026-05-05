555f258c8349bde95823986c981e5969c7cc9989
seid ahmetovic <107319842+seyyy0@users.noreply.github.com>
2026-05-05 10:53:29 +0200

    Template cleanup, rename files and remove default code

--- src/wasmWasiTest/kotlin/Test.kt
+++ src/wasmWasiTest/kotlin/Test.kt
@@ -4,8 +4,5 @@ import kotlin.test.assertTrue
 class WasiTest {
     @Test
     fun mainTest() {
-        val monotonicTime1 = wasiMonotonicTime()
-        val monotonicTime2 = wasiMonotonicTime()
-        assertTrue(monotonicTime1 <= monotonicTime2, "Wasi monotonic clock is not monotonic :(")
     }
 }
