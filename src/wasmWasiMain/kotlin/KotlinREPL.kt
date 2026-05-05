43b64c73390d3225456cf42c6b95972bc33bbaa4
seid ahmetovic <107319842+seyyy0@users.noreply.github.com>
2026-05-05 12:24:06 +0200

    Removed magic numbers, added EOF check for I/O, added more robust trimming of input (for Windows)

--- src/wasmWasiMain/kotlin/KotlinREPL.kt
+++ src/wasmWasiMain/kotlin/KotlinREPL.kt
@@ -2,12 +2,14 @@ import kotlin.wasm.unsafe.Pointer
 import kotlin.wasm.unsafe.UnsafeWasmMemoryApi
 import kotlin.wasm.unsafe.withScopedMemoryAllocator
 
+const val BUFFER_SIZE = 256
+
 fun main() {
     println("Hello from Kotlin via WASI. REPL program is being initialized.")
     println("Instructions:\n" +
             "  - Type 'quit' to exit\n")
     while (true) {
-        val input = wasiReadLine()
+        val input = wasiReadLine() ?: break // If EOF (eg. CTRL+D)
         if (input == "quit") {
             println("Goodbye!")
             break
@@ -16,15 +18,16 @@ fun main() {
     }
 }
 
+@OptIn(ExperimentalWasmInterop::class)
 @WasmImport("wasi_snapshot_preview1", "fd_read")
 private external fun wasiRawFdRead(fd: Int, iovs_ptr: Int, iovs_len: Int, nread_ptr: Int): Int
 
 @OptIn(UnsafeWasmMemoryApi::class)
-fun wasiReadLine(): String = withScopedMemoryAllocator { allocator ->
-    val buff = allocator.allocate(256)
+fun wasiReadLine(): String? = withScopedMemoryAllocator { allocator ->
+    val buff = allocator.allocate(BUFFER_SIZE)
     val iovec = allocator.allocate(8)
     Pointer(iovec.address).storeInt(buff.address.toInt())
-    Pointer(iovec.address + 4u).storeInt(256)
+    Pointer(iovec.address + 4u).storeInt(BUFFER_SIZE)
 
     val nread = allocator.allocate(4)
     val ret = wasiRawFdRead(
@@ -35,10 +38,12 @@ fun wasiReadLine(): String = withScopedMemoryAllocator { allocator ->
     )
     check(ret == 0) {"fd_read failed with code $ret"}
     val bytesRead = Pointer(nread.address).loadInt()
+    if (bytesRead == 0) return@withScopedMemoryAllocator null // Check for EOF
+
     buildString {
         for (i in 0 until bytesRead) {
             append(Pointer(buff.address + i.toUInt()).loadByte().toInt().toChar())
         }
-    }.trim()
+    }.trimEnd('\n', '\r')
 }
 
